40c0c15da13e427cc1b9936e95d1d32917fb1d82
Robert Leon <llexi@hotmail.com>
2014-04-01 19:05:13 +0100

    Reworked if/else with ternary operator and edited for single user input

--- makers.rb
+++ makers.rb
@@ -1,19 +1,21 @@
 # Assign the string "Makers" to a variable and print it out
 
-people = "Makers"
+user = "Maker"
 puts "The people variable is currently: #{people}"
 
 # Change the variable to "Coders" and print it again
 
-people = "Coders"
+user = "Coder"
 puts "The people variable is currently #{people}"
 
 print "How old are you? "
 
 age = gets.rstrip
 
-if age.empty?
-	puts "#{people}, I have no idea how old you are!"
-else 
-	puts "#{people}, you are #{age} years old!"
-end
+# if age.empty?
+# 	puts "#{people}, I have no idea how old you are!"
+# else 
+# 	puts "#{people}, you are #{age} years old!"
+# end
+
+age.empty? ? puts("#{user}, I have no idea how old you are!") : puts("#{user}, you are #{age} years old!")
